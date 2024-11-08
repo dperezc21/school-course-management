d9210a61df377a1604b35babedb54f18931be886
davier enrique perez cabrera <davierperez11.2@gmail.com>
2024-11-08 16:24:19 -0500

    refactor: use constant student table

--- src/controllers/student.controller.ts
+++ src/controllers/student.controller.ts
@@ -4,13 +4,14 @@ import { ConnectionInterface } from "../interfaces/connectionInterface";
 
 import { ConnectionMysql } from '../data-base/connection.mysql';
 import {MysqlError} from "mysql";
+import {STUDENT_TABLE} from "../constants/table-names";
 
 const {getConnection}: ConnectionInterface = new ConnectionMysql();
 
 export class StudentController {
     getStudentById(req: Request, res: Response) {
         const studentId: string = req.params.id;
-        const queryUrl: string = `select * from student where id=${studentId}`;
+        const queryUrl: string = `select * from ${STUDENT_TABLE} where id=${studentId}`;
         getConnection().query(queryUrl, (err: MysqlError, result: []) => {
 
             if(err) res.status(500).json({
@@ -25,8 +26,10 @@ export class StudentController {
 
     createStudent(req: Request, res: Response) {
 
-        const student: StudentModel = req.body;
-        const queryUrl: string = `INSERT INTO student (first_name, last_name, career, image_url) VALUES ('${student.firstName}', '${student.lastName}', '${student.career}', '${student.imageUrl}')`;
+        const { firstName, lastName, career, imageUrl }: StudentModel = req.body;
+        const queryUrl: string = `INSERT INTO ${STUDENT_TABLE} 
+                                    (first_name, last_name, career, image_url) 
+                                    VALUES ('${firstName}', '${lastName}', '${career}', '${imageUrl}')`;
         getConnection().query(queryUrl,
             (err) => {
                 if(err) res.status(500).json({
@@ -39,7 +42,7 @@ export class StudentController {
     }
 
     getAllStudents(req: Request, res: Response) {
-        getConnection().query("select * from student", (err: MysqlError, result: []) =>{
+        getConnection().query("select * from ${STUDENT_TABLE}", (err: MysqlError, result: []) =>{
             if(err) res.status(500).json({
                 message: "Error de base de datos"
             })
@@ -53,7 +56,12 @@ export class StudentController {
     updateStudent(req: Request, res: Response){
         const {firstName, lastName, career, imageUrl}: StudentModel = req.body;
         const studentId: string = req.params.id;
-        const queryUrl: string = `UPDATE student SET first_name='${firstName}', last_name='${lastName}', career='${career}' ${imageUrl ? `, image_url='${imageUrl}'` : ''} where id=${studentId}`;
+        const queryUrl: string = `UPDATE ${STUDENT_TABLE} 
+                                    SET first_name='${firstName}', 
+                                        last_name='${lastName}', 
+                                        career='${career}' 
+                                        ${imageUrl ? `, image_url='${imageUrl}'` : ''}
+                                        where id=${studentId}`;
         getConnection().query(queryUrl, (err: MysqlError) => {
            if(err) res.status(500).json({
                message: "Error al actualizar estudiante"
